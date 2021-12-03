5a9e9107fa006a7454532f7927301bbdfce48562
Travis CI <travis@travis-ci.org>
2021-12-03 16:38:02 +0000

    completed contain function module1

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -12,7 +12,9 @@ return x===y && j===k;
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return this.some((c)=>same(c,cell));
+}
 
 const printCell = (cell, state) => {};
 
